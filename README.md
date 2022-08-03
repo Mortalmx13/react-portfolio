fee4730fce1f63e3bfad9de1659dc55e44eed59c
Charles Young <ctyoungiv@gmail.com>
2022-08-02 23:11:20 -0400

    worked on readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-portfolio
+
+# Table of Contents
+1. [Description](#description)
+2. [Visuals](#visuals)
+3. [Resources](#resources)
+
+## Description
+
+This assignmant was designed to make a portfolio where there is a task to show the reader what projects I have accomplished and some information about me. This is also to show that I cna set up links and use react confritably to make different js files to run at the same time.
+
+## Visuals
+![Portfolio](./React-App%20image.png)
+
+## Resources
+- [Live Site](https://mortalmx13.github.io/portfolio/)
+
+- [Respository](https://github.com/Mortalmx13/react-portfolio)
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
         // bottom boorder and links to my github and linkedin
     <footer className="footer bg-dark fixed-bottom">
         <a href="https://github.com/Mortalmx13"><img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="Github" className="icon"/></a>
+        <a href="https://twitter.com/CTYoung10/photo"><img src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="Twitter" className="icon"/></a>
         <a href="https://www.linkedin.com/in/charles-young-ab129623a/"><img src="https://yt3.ggpht.com/9XmuxL_LL7CxAOOlbBgTnJIo2uHpoLKHhWzlPt7O49ULQmvBSJlxk1RpX3pJ8jkRBkD6p9BIRg=s900-c-k-c0x00ffffff-no-rj" alt="LinkedIn" className="icon"/></a>
     </footer>
     )
